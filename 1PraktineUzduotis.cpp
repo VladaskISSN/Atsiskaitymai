31328b011cd3f95d2f6ef3bbd7203824ea175fbb
Vladas <vladask93@gmail.com>
2026-03-09 19:36:37 +0200

    Kuro palyginimo kiekio panaikinimas

--- 1PraktineUzduotis.cpp
+++ 1PraktineUzduotis.cpp
@@ -15,14 +15,16 @@ int main() {
     cout << "Pasirink valiuta (1-GBP, 2-USD, 3-INR): ";
     cin >> Valiuta;
 
-    cout << "Ivesk kieki: ";
-    cin >> Kiekis;
 
     if (Pasirinkimas == 1) {
         if (Valiuta == 1) cout << "1 EUR = " << 0.8729 << " GBP";
         if (Valiuta == 2) cout << "1 EUR = " << 1.1793 << " USD";
         if (Valiuta == 3) cout << "1 EUR = " << 104.6918 << " INR";
+        return 0;
     }
+
+    cout << "Ivesk kieki: ";
+    cin >> Kiekis;
 
     if (Pasirinkimas == 2) {
         if (Valiuta == 1) Rezultatas = Kiekis * 0.8600;
