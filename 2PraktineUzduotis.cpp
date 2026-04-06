6a6ddfb6ebd68cc4d50e860a4c56cf25c04537f3
Vladas <vladask93@gmail.com>
2026-04-06 18:32:21 +0300

    Bibliotekos, konstantos ir masyvai, kurie bus kuriami vardams ir pazymiams saugoti

---
+++ 2PraktineUzduotis.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int DidziausiasgGalimasMokiniuskaicius = 100;
+const int DidziausiasGalimasPazymiuSkaicius = 10;
+string Vardai[DidziausiasgGalimasMokiniuskaicius];
+int Pazymiai[DidziausiasgGalimasMokiniuskaicius][DidziausiasGalimasPazymiuSkaicius];
+int KiekPazymiu[DidziausiasgGalimasMokiniuskaicius];
+int KiekMokiniu = 0;
+
+
+
+
+
+
