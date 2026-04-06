33c142acbbef3c6b9c6c2189b9955e1c2fbb2881
Vladas <vladask93@gmail.com>
2026-04-06 18:48:11 +0300

    Menu ir ciko irasymas, ciklas baigiasi tik ivedus 0

--- 2PraktineUzduotis.cpp
+++ 2PraktineUzduotis.cpp
@@ -9,8 +9,21 @@ int Pazymiai[DidziausiasgGalimasMokiniuskaicius][DidziausiasGalimasPazymiuSkaici
 int KiekPazymiu[DidziausiasgGalimasMokiniuskaicius];
 int KiekMokiniu = 0;
 
+int main() {
+    int pasirinkimas;
+do {
+    cout << "Mokiniu pazymiu sistema\n";
+    cout << "1 - Ivesti mokini\n";
+    cout << "2 - Parodyti mokiniu sarasa\n";
+    cout << "3 - Pakeisti mokinio pazymi\n";
+    cout << "4 - Istrinti mokini is saraso\n";
+    cout << "5 - Uzdaryti programa\n";
+    cout << "pasirink";
+    cin >> pasirinkimas;
 
-
+} while (pasirinkimas != 0);
+return 0;
+}
 
 
 
